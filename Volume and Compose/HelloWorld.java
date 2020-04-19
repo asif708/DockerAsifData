0c0f1a83fb496e93599c0d9a8fce99a48edd0a9b
asif immanad <asif_immanad@persistent.co.in>
2020-04-20 04:07:42 +0530

    DOcker File, Volume and Compose

---
+++ Volume and Compose/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	float f = 3.14f;
+	public static void main(String[] abc) {
+
+		String str1 = abc[0];
+		String str2 = abc[1];
+		
+		System.out.println("Hello World"+str1+str2);
+	}
+}
